4e43e1639ff9b30df5ba9b3e0d216717a943b43b
gayashan4lk <gayashan4lk@gmail.com>
2025-04-02 22:29:46 +0530

    create question api

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+import models
+from database import engine, SessionLocal
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+class ChoicesBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+
+class QuestionsBase(BaseModel):
+    question_text: str
+    choices: list[ChoicesBase]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/questions/")
+async def create_question(question: QuestionsBase, db: db_dependency):
+    db_question = models.Questions(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choices(
+            question_id=db_question.id,
+            choice_text=choice.choice_text,
+            is_correct=choice.is_correct
+        )
+        db.add(db_choice)
+    db.commit()
+    db.refresh(db_choice)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
